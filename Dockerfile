5ffa419ccd4be92e3fd2dc030944d02f1f341397
yogesh <yogeshgowda24@gmail.com>
2025-12-26 10:46:27 +0530

    commmit yogesh

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+# Stage 1: Build React app
+FROM node:22.11.0 AS build
+WORKDIR /app
+COPY . .
+RUN npm install --legacy-peer-deps \
+  && npm run build \
+  && cp -r build server \
+  && cd server \
+  && npm install --legacy-peer-deps
+
+
+# Stage 2: Rnunning the BFF Server
+FROM node:22-alpine AS final
+WORKDIR /server
+COPY --from=build /app/server .
+EXPOSE 5000
+CMD ["node", "index.js"]
---
+++ server/index.js
@@ -0,0 +1,48 @@
+import express from "express";
+import fetch from "node-fetch"; // if you need node-fetch
+import path from "path"; // Ensure the path module is imported
+const app = express();
+import bodyParser from "body-parser";
+import cors from "cors";
+import axios from 'axios'; 
+
+// Enable CORS
+app.use(cors({
+    origin: "*", // Allow requests from this origin
+    methods: ["GET", "POST"], // Specify allowed HTTP methods
+    allowedHeaders: ["Content-Type"] // Specify allowed headers
+}));
+
+// Serve static files from the React app's build directory
+const __dirname = path.resolve(); // Required for ES module compatibility
+app.use(express.static(path.join(__dirname, "build")));
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Proxy route to the backend
+app.post('/customer/customerDetails', async (req, res) => {
+    try {
+      const data = req.body;
+      // const response = await axios.post('https://backend.hyderabad-packers-movers.in/customer/customerDetails', data);
+      const response = await axios.post('http://18.191.140.225:8080/customer/customerDetails', data);
+      // Send the JSON response from Spring Boot to the frontend
+      res.status(200).json(response.data);
+    } catch (error) {
+      console.error("Error communicating with Spring Boot:", error.message);
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
+  });
+
+  // Catch-all route to serve the React app for non-API requests
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+
+// Start the BFF server
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`BFF server running on http://localhost:${PORT}`);
+});
